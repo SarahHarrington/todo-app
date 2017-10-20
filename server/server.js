f56240503a238d224efc8572ea59fc1f8c53e98b
Sarah Harrington <Sarah.Soberg@gmail.com>
2017-10-20 16:41:02 -0500

    server up, jquery and javascript loaded

---
+++ server/server.js
@@ -0,0 +1,14 @@
+//server set up variables
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 5000;
+
+//app stuff
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+
+app.listen(port, function(){
+    console.log('listening on port', port)
+})
